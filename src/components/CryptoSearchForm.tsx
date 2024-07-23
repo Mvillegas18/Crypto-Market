01f356363e6b6cc8ced3652c09524c7bcdecb420
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-23 17:54:57 -0500

    feat: Add form validation to CryptoSearchForm component

--- src/components/CryptoSearchForm.tsx
+++ src/components/CryptoSearchForm.tsx
@@ -1,13 +1,48 @@
+import { useState } from 'react';
 import { currencies } from '../data';
+import { useCryptoStore } from '../store';
+import { PairCurrency } from '../types';
+import ErrorMessage from './ErrorMessage';
 
 export default function CryptoSearchForm() {
+	const { cryptoCurrencies } = useCryptoStore();
+	const [pair, setPair] = useState<PairCurrency>({
+		currency: '',
+		cryptocurrency: '',
+	});
+
+	const [error, seterror] = useState('');
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (Object.values(pair).includes('')) {
+			seterror('Todos los campos son obligatorios');
+			return;
+		}
+
+		// Consultar la api
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setPair({
+			...pair,
+			[e.target.name]: e.target.value,
+		});
+	};
+
 	return (
-		<form className='form'>
+		<form
+			className='form'
+			onSubmit={handleSubmit}>
+			{error && <ErrorMessage>{error}</ErrorMessage>}
+
 			<div className='field'>
 				<label htmlFor='currency'>Moneda:</label>
 				<select
 					name='currency'
-					id='currency'>
+					id='currency'
+					onChange={handleChange}
+					value={pair.currency}>
 					<option value=''>-- Seleccione --</option>
 					{currencies.map((currency) => (
 						<option
@@ -23,8 +58,17 @@ export default function CryptoSearchForm() {
 				<label htmlFor='cryptocurrency'>Cryptomoneda:</label>
 				<select
 					name='cryptocurrency'
-					id='cryptocurrency'>
+					id='cryptocurrency'
+					onChange={handleChange}
+					value={pair.cryptocurrency}>
 					<option value=''>-- Seleccione --</option>
+					{cryptoCurrencies.map(({ CoinInfo }) => (
+						<option
+							key={CoinInfo.Id}
+							value={CoinInfo.Name}>
+							{CoinInfo.FullName}
+						</option>
+					))}
 				</select>
 			</div>
 
