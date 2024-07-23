4268240992ad5bdba658ad21f35c14975f12d7fc
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-23 17:13:04 -0500

    feat: Add CryptoService to fetch cryptocurrency data

--- src/store.ts
+++ src/store.ts
@@ -1,28 +1,12 @@
-import axios from 'axios';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
+import { getCryptos } from './services/CryptoService';
 import { CryptoCurrency } from './types';
 
 type CryptoStore = {
 	cryptoCurrencies: CryptoCurrency[];
 	fetchCryptos: () => Promise<void>;
 };
-
-async function getCryptos() {
-	const url =
-		'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=20&tsym=USD';
-
-	const {
-		data: { Data },
-	} = await axios(url);
-
-	const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-
-	if (result.success) {
-		return result.data;
-	}
-}
 
 export const useCryptoStore = create<CryptoStore>()(
 	devtools((set) => ({
