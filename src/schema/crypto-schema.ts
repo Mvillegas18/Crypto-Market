66f35b9b426ecccc3ad1f8c6d61f967f81e5ee11
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-23 16:49:38 -0500

    feat: Refactor CryptoCurrencyResponseSchema in crypto-schema.ts
    This commit refactors the `CryptoCurrencyResponseSchema` in the `crypto-schema.ts` file. The schema is updated to match the new structure of the response data for a single cryptocurrency. This change ensures that the response data is properly validated and conforms to the expected format.

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -5,14 +5,12 @@ export const CurrencySchema = z.object({
 	name: z.string(),
 });
 
-export const CryptoCurrencyResponseSchema = z.array(
-	z.object({
-		CoinInfo: z.object({
-			FullName: z.string(),
-			Name: z.string(),
-		}),
-	})
-);
+export const CryptoCurrencyResponseSchema = z.object({
+	CoinInfo: z.object({
+		FullName: z.string(),
+		Name: z.string(),
+	}),
+});
 
 export const CryptoCurrenciesResponseSchema = z.array(
 	CryptoCurrencyResponseSchema
