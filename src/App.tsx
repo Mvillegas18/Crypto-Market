9ca917aec0508882c482f4fb194c8bc95934832e
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-22 21:30:17 -0500

    feat: Add Google Fonts and update landing page layout
    This commit adds Google Fonts to the project by including the necessary links in the HTML file. It also updates the layout of the landing page in the App component, adding a container, an app title with a span for the word "Criptomonedas", and a content section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
 export default function App() {
-	return <h1>Hola mundo</h1>;
+	return (
+		<div className='container'>
+			<h1 className='app-title'>
+				Cotizador de <span>Criptomonedas</span>
+			</h1>
+
+			<div className='content'></div>
+		</div>
+	);
 }
 
