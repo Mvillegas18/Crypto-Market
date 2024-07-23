1a426e41699e6cc3e46348a7b2ed0b5ffd2dc955
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-23 16:36:23 -0500

    feat: Add CryptoCurrenciesResponseSchema to crypto-schema.ts
    This commit adds the `CryptoCurrenciesResponseSchema` to the `crypto-schema.ts` file. The schema defines the structure of the response data for multiple cryptocurrencies. It ensures that the response data is validated and conforms to the expected format.

--- src/schema/crypto-schema.ts
+++ src/schema/crypto-schema.ts
@@ -4,3 +4,16 @@ export const CurrencySchema = z.object({
 	code: z.string(),
 	name: z.string(),
 });
+
+export const CryptoCurrencyResponseSchema = z.array(
+	z.object({
+		CoinInfo: z.object({
+			FullName: z.string(),
+			Name: z.string(),
+		}),
+	})
+);
+
+export const CryptoCurrenciesResponseSchema = z.array(
+	CryptoCurrencyResponseSchema
+);
