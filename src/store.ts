168eeb1ef8800618e59c50fb0ea3571aa967766a
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-23 16:49:30 -0500

    feat: Add useCryptoStore to manage cryptocurrency data
    This commit adds the `useCryptoStore` hook to manage cryptocurrency data in the application. The `useCryptoStore` hook fetches the top 20 cryptocurrencies from the CryptoCompare API and returns the data. This will allow other components to easily access and use the cryptocurrency data.

---
+++ src/store.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { create } from 'zustand';
+import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
+
+async function getCryptos() {
+	const url =
+		'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=20&tsym=USD';
+
+	const {
+		data: { Data },
+	} = await axios(url);
+
+	const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+
+	if (result.success) {
+		return result.data;
+	}
+}
+
+export const useCryptoStore = create(() => ({
+	fetchCryptos: async () => {
+		const cryptoCurrencies = await getCryptos();
+
+		return cryptoCurrencies;
+	},
+}));
