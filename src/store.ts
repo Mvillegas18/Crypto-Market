6befe8ec09a6463320298b0e52796b30e2b2d51b
Miguel Angel Villegas <chumilo227@gmail.com>
2024-07-24 08:58:56 -0500

    feat: Add result property to CryptoStore
    This commit adds a new property called `result` to the `CryptoStore` type in the `store.ts` file. The `result` property is of type `CryptoPriceSchema` and is used to store the fetched data from the API, including the image URL, last update time, price, high day value, low day value, and 24-hour change percentage. This enhancement improves the functionality of the application by providing access to the latest cryptocurrency price data.

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { fetchCurrentCryptoPrice, getCryptos } from './services/CryptoService';
-import { CryptoCurrency, PairCurrency } from './types';
+import { CryptoCurrency, CryptoPriceSchema, PairCurrency } from './types';
 
 type CryptoStore = {
 	cryptoCurrencies: CryptoCurrency[];
+	result: CryptoPriceSchema;
 	fetchCryptos: () => Promise<void>;
 	fetchData: (pair: PairCurrency) => Promise<void>;
 };
@@ -12,6 +13,14 @@ type CryptoStore = {
 export const useCryptoStore = create<CryptoStore>()(
 	devtools((set) => ({
 		cryptoCurrencies: [],
+		result: {
+			IMAGEURL: '',
+			LASTUPDATE: '',
+			PRICE: '',
+			HIGHDAY: '',
+			LOWDAY: '',
+			CHANGEPCT24HOUR: '',
+		},
 		fetchCryptos: async () => {
 			const cryptoCurrencies = await getCryptos();
 
@@ -20,7 +29,10 @@ export const useCryptoStore = create<CryptoStore>()(
 			}));
 		},
 		fetchData: async (pair) => {
-			await fetchCurrentCryptoPrice(pair);
+			const result = await fetchCurrentCryptoPrice(pair);
+			set(() => ({
+				result,
+			}));
 		},
 	}))
 );
